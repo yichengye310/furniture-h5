21292b32922323737c6de0ed088d4b9848dee0ac
Yicheng Ye <ye.yicheng123@gmail.com>
2020-10-26 14:45:46 -0400

    bug fix

--- src/pages/home/order/Paymentmethod.vue
+++ src/pages/home/order/Paymentmethod.vue
@@ -10,18 +10,18 @@
             </div>
 
             <div class="choose">选择支付方式</div>
-            <!--        <div class="payment-weixin pay" @click="Alipaypayment()">
+            <div @click="Alipaypayment()" class="payment-weixin pay">
                 <div class="radio">
-                    <div class="radio-n" :class="{active:isactive===1}"></div>
+                    <div :class="{active:isactive===1}" class="radio-n"></div>
                 </div>
 
                 <div class="pay-w pay1">
-                  <div class="pay-icon">
-                    <img src="../../../assets/images/user/orders/zhifubao.png" alt="">
-                  </div>
-                  <div>支付宝支付</div>
+                    <div class="pay-icon">
+                        <img alt="" src="../../../assets/images/user/orders/zhifubao.png">
+                    </div>
+                    <div>支付宝支付</div>
                 </div>
-            </div>-->
+            </div>
             {{opya}}
             <div @click="WeChatpayment()" class="payment-zhifubao pay">
                 <div class="radio">
@@ -71,7 +71,8 @@
                 ordertheid: [],
                 roomName: localStorage.getItem("roomName"),
                 userName: localStorage.getItem("phoneNumber"),
-                userType: localStorage.getItem("userType2")
+                userType: localStorage.getItem("userType2"),
+                channels: ""
             };
         },
         computed: {
@@ -103,10 +104,20 @@
         },
         beforeCreate() {
             document.addEventListener("plusready", function () {
+                this.plusReady();
             }, false);
         },
 
         methods: {
+            plusReady() {
+                // 获取支付通道
+                plus.payment.getChannels(function (channels) {
+                    this.channels = channels;
+                    console.log(JSON.stringify(channels));
+                }, function (e) {
+                    alert("获取支付通道失败：" + e.message);
+                });
+            },
             gouwuchetijiao() {
                 let bodygoumai = [];
                 let v = [];
@@ -165,13 +176,15 @@
                 this.istabfixed = -position.y > this.tabberoffsettop;
             },
             sub() {
+                let WXPAY_SERVER = this.$config.baseApi + "/wxPay/pay";
+                let ALIPAY_SERVER = this.$config.baseApi + "/zfbPay/toPayApp";
                 let bodyall = {};
                 if (this.$route.query.cartdata === "1") {
                     let body = {
                         productList: this.bodygoumai,
                         userID: localStorage["uid"],
                         userRemark: sessionStorage["value"],
-                        favourMode: this.couponAmount > 0? 1: 0,
+                        favourMode: this.couponAmount > 0 ? 1 : 0,
                         favourAmount: (this.couponAmount || 0) * 100,
                         favourRelatedCouponID: 0,
                         transportationExpense: 0,
@@ -186,7 +199,7 @@
                         productList: this.dandugoumai,
                         userID: localStorage["uid"],
                         userRemark: sessionStorage["value"],
-                        favourMode: this.couponAmount > 0? 1: 0,
+                        favourMode: this.couponAmount > 0 ? 1 : 0,
                         favourAmount: (this.couponAmount || 0) * 100,
                         favourRelatedCouponID: 0,
                         transportationExpense: 0,
@@ -208,18 +221,21 @@
 
                     axios({
                         method: "post",
-                        url: this.$config.baseApi + "/wxPay/H5pay",
+                        url: this.isactive === 1? ALIPAY_SERVER: WXPAY_SERVER,
                         headers: {
                             "Content-Type": "application/json;"
                         },
                         data: JSON.stringify(orderIDList)
                     }).then(res => {
-                        plus.webview.create(res.data.data.mweb_url,
-                            "微信支付", {
-                                additionalHttpHeaders: {Referer: "http://www.ammsshop.com"}
+                        let channel = "";
+                        if (channel)
+                        plus.payment.request(channel,res.data,function(result){
+                            plus.nativeUI.alert("支付成功！",function(){
+
                             });
-                        console.log(JSON.stringify(res));
-                        this.$router.replace("/order/end?out_trade_no=" + res.data.data.out_trade_no + "&orderID=" + this.$route.query.orderID);
+                        },function(error){
+                            plus.nativeUI.alert("支付失败：" + error.code);
+                        });
                     });
                 } else {
                     axios({
@@ -241,7 +257,7 @@
                                 spbillCreateIp: returnCitySN["cip"],
                                 type: 1,
                                 userID: localStorage["uid"],
-                                totalAmount:res.data.data.totalAmount
+                                totalAmount: res.data.data.totalAmount
                             };
                             // console.log(returnCitySN["cip"])
                             Toast("订单提交成功");
@@ -256,8 +272,8 @@
                             }).then(res => {
                                 plus.webview.create(res.data.data.mweb_url,
                                     "微信支付", {
-                                    additionalHttpHeaders: {Referer: "http://www.ammsshop.com"}
-                                });
+                                        additionalHttpHeaders: {Referer: "http://www.ammsshop.com"}
+                                    });
                                 console.log(JSON.stringify(res));
                                 this.$router.replace("/order/end?out_trade_no=" + res.data.data.out_trade_no + "&orderID=" + res.data.data.orderID);
                                 // if (res.err_msg == "get_brand_wcpay_request:ok") {
--- src/store/modules/cart/index.js
+++ src/store/modules/cart/index.js
@@ -1,127 +1,128 @@
 import Vue from 'vue';
 import {getCartData} from "../../../api/goods/cart.js";
+
 export default {
-    namespaced:true,
-    state:{
-        cartData:localStorage['cartData']?JSON.parse(localStorage['cartData']):[],
-		orderBuy:localStorage['orderBuy']?JSON.parse(localStorage['orderBuy']):[],
-		cartsorderdata:[],
-		cartsgoods:[],
-		price:0,
+    namespaced: true,
+    state: {
+        cartData: localStorage['cartData'] ? JSON.parse(localStorage['cartData']) : [],
+        orderBuy: localStorage['orderBuy'] ? JSON.parse(localStorage['orderBuy']) : [],
+        cartsorderdata: [],
+        cartsgoods: [],
+        price: 0,
         couponAmount: 0,
         couponID: undefined
     },
-    mutations:{
-		["GET_GOODSPRICE"](state,payload){
-			let v = []
-			let z = 0
-			for(let i=0;i<state.orderBuy.length;i++){
-				z = state.orderBuy[i].amount
-				for(let j=0;j<state.orderBuy[i].attrs.length;j++){
-					v.push( state.orderBuy[i].attrs[j].param[0].bandPrice*0.01 * z)
-				}
-			}
-			var s = 0
-			v.forEach(function(val, idx, v) {
-					s += val;
-			}, 0);
-			return state.price = s
+    mutations: {
+        ["GET_GOODSPRICE"](state, payload) {
+            let v = []
+            let z = 0
+            for (let i = 0; i < state.orderBuy.length; i++) {
+                z = state.orderBuy[i].amount
+                for (let j = 0; j < state.orderBuy[i].attrs.length; j++) {
+                    v.push(state.orderBuy[i].attrs[j].param[0].bandPrice * 0.01 * z)
+                }
+            }
+            var s = 0
+            v.forEach(function (val, idx, v) {
+                s += val;
+            }, 0);
+            return state.price = s
 
 
-		},
-		//查询购物车商品
-		["GET_CART"](state,payload){
-			state.cartsgoods=payload.cartsgoods;
-		},
-		["ADD_CARTPAGE"](state,payload){
-			if(payload.cartsgoods.length>0){
-				for (let i=0;i<payload.cartsgoods.length;i++) {
-					state.cartsgoods.push(payload.cartsgoods[i])
-				}
-			}
-		},
+        },
+        //查询购物车商品
+        ["GET_CART"](state, payload) {
+            state.cartsgoods = payload.cartsgoods;
+        },
+        ["ADD_CARTPAGE"](state, payload) {
+            if (payload.cartsgoods.length > 0) {
+                for (let i = 0; i < payload.cartsgoods.length; i++) {
+                    state.cartsgoods.push(payload.cartsgoods[i])
+                }
+            }
+        },
 
-		//订单总价计算
-		["SUM_TOORDER"](state,payload){
-				state.price = payload.price
-		},
+        //订单总价计算
+        ["SUM_TOORDER"](state, payload) {
+            state.price = payload.price
+        },
 
-		//直接购买商品
-		 ["ADD_ORDER"](state,payload){
-			 state.orderBuy=[payload.orderBuy]
-		 },
+        //直接购买商品
+        ["ADD_ORDER"](state, payload) {
+            state.orderBuy = [payload.orderBuy]
+        },
 
-		 ["ADD_CARTORDER_GOODSPRICE"](state,payload){
-				let v = []
-				let z = 0
-				for(let i=0;i<state.cartsorderdata.length;i++){
-					z = state.cartsorderdata[i].cartData.amount
-					for(let j=0;j<state.cartsorderdata[i].cartData.attrs.length;j++){
-						v.push(state.cartsorderdata[i].cartData.attrs[j].param[0].bandPrice * z)
-					}
-				}
-				var s = 0
-				v.forEach(function(val, idx, v) {
-						s += val;
-				}, 0);
-				return state.price = s
-		 },
-		 //购物车直接购买
-		 ["ADD_CARTORDER"](state,payload){
-				state.cartsorderdata = payload.cartsorderdata
-		 },
+        ["ADD_CARTORDER_GOODSPRICE"](state, payload) {
+            let v = []
+            let z = 0
+            for (let i = 0; i < state.cartsorderdata.length; i++) {
+                z = state.cartsorderdata[i].cartData.amount
+                for (let j = 0; j < state.cartsorderdata[i].cartData.attrs.length; j++) {
+                    v.push(state.cartsorderdata[i].cartData.attrs[j].param[0].bandPrice * z)
+                }
+            }
+            var s = 0
+            v.forEach(function (val, idx, v) {
+                s += val;
+            }, 0);
+            return state.price = s
+        },
+        //购物车直接购买
+        ["ADD_CARTORDER"](state, payload) {
+            state.cartsorderdata = payload.cartsorderdata
+        },
         //添加购物车商品
-        ["ADD_ITEM"](state,payload){
-			state.cartData=payload.cartData;
-            let isSame=false;
-            if(state.cartData.length>0){
-                for(let i=0;i<state.cartData.length;i++){
-                    if(state.cartData[i].gid===payload.cartData.gid && JSON.stringify(state.cartData[i].attrs)===JSON.stringify(payload.cartData.attrs)){
-                        isSame=true;
-                        state.cartData[i].amount=parseInt(state.cartData[i].amount)+parseInt(payload.cartData.amount)
+        ["ADD_ITEM"](state, payload) {
+            state.cartData = payload.cartData;
+            let isSame = false;
+            if (state.cartData.length > 0) {
+                for (let i = 0; i < state.cartData.length; i++) {
+                    if (state.cartData[i].gid === payload.cartData.gid && JSON.stringify(state.cartData[i].attrs) === JSON.stringify(payload.cartData.attrs)) {
+                        isSame = true;
+                        state.cartData[i].amount = parseInt(state.cartData[i].amount) + parseInt(payload.cartData.amount)
                         break;
                     }
                 }
             }
-            if(!isSame){
+            if (!isSame) {
                 state.cartData.push(payload.cartData);
             }
-            localStorage['cartData']=JSON.stringify(state.cartData);
+            localStorage['cartData'] = JSON.stringify(state.cartData);
         },
         //删除商品
-        ["DEL_ITEM"](state,payload){
-            state.cartData.splice(payload.index,1);
-            localStorage['cartData']=JSON.stringify(state.cartData);
+        ["DEL_ITEM"](state, payload) {
+            state.cartData.splice(payload.index, 1);
+            localStorage['cartData'] = JSON.stringify(state.cartData);
         },
         //更改数量
-        ["SET_AMOUNT"](state,payload){
-            state.cartData[payload.index].amount=payload.amount;
-            state.cartData[payload.index].amount=parseInt(state.cartData[payload.index].amount.replace(/[^\d]/g,""))
-            if(!state.cartData[payload.index].amount){
-                state.cartData[payload.index].amount=1
+        ["SET_AMOUNT"](state, payload) {
+            state.cartData[payload.index].amount = payload.amount;
+            state.cartData[payload.index].amount = parseInt(state.cartData[payload.index].amount.replace(/[^\d]/g, ""))
+            if (!state.cartData[payload.index].amount) {
+                state.cartData[payload.index].amount = 1
             }
-            localStorage['cartData']=JSON.stringify(state.cartData);
+            localStorage['cartData'] = JSON.stringify(state.cartData);
         },
         //增加数量
-        ["INC_AMOUNT"](state,payload){
-            state.cartData[payload.index].amount+=1;
-            Vue.set(state.cartData,payload.index,state.cartData[payload.index]);
-            localStorage['cartData']=JSON.stringify(state.cartData);
+        ["INC_AMOUNT"](state, payload) {
+            state.cartData[payload.index].amount += 1;
+            Vue.set(state.cartData, payload.index, state.cartData[payload.index]);
+            localStorage['cartData'] = JSON.stringify(state.cartData);
         },
         //减少数量
-        ["DEC_AMOUNT"](state,payload){
-            state.cartData[payload.index].amount=state.cartData[payload.index].amount>1?--state.cartData[payload.index].amount:1;
-            Vue.set(state.cartData,payload.index,state.cartData[payload.index]);
-            localStorage['cartData']=JSON.stringify(state.cartData);
+        ["DEC_AMOUNT"](state, payload) {
+            state.cartData[payload.index].amount = state.cartData[payload.index].amount > 1 ? --state.cartData[payload.index].amount : 1;
+            Vue.set(state.cartData, payload.index, state.cartData[payload.index]);
+            localStorage['cartData'] = JSON.stringify(state.cartData);
         },
         //选择商品
-        ["SELECT_ITEM"](state,payload){
-            state.cartData[payload.index].checked=!state.cartData[payload.index].checked;
-            Vue.set(state.cartData,payload.index,state.cartData[payload.index]);
-            localStorage['cartData']=JSON.stringify(state.cartData);
+        ["SELECT_ITEM"](state, payload) {
+            state.cartData[payload.index].checked = !state.cartData[payload.index].checked;
+            Vue.set(state.cartData, payload.index, state.cartData[payload.index]);
+            localStorage['cartData'] = JSON.stringify(state.cartData);
         },
         //全选/反选
-        ["ALL_SELECT_ITEM"](state,payload) {
+        ["ALL_SELECT_ITEM"](state, payload) {
             if (state.cartData.length > 0) {
                 for (let i = 0; i < state.cartData.length; i++) {
                     state.cartData[i].checked = payload.checked;
@@ -131,77 +132,75 @@ export default {
         },
 
         ["APPLY_COUPON"](state, payload) {
-		    console.log(payload.couponAmount);
-		    state.price -= payload.couponAmount;
-		    state.price = Math.max(state.price, 0);
-		    state.couponAmount = payload.couponAmount;
-		    this.couponID = payload.couponID
+            console.log(payload.couponAmount);
+            state.price -= payload.couponAmount;
+            state.price = Math.max(state.price, 0);
+            state.couponAmount = payload.couponAmount;
+            this.couponID = payload.couponID
         }
     },
-	actions:{
-		getCartGoods(conText,payload){
-		    getCartData(conText.rootState.user.uid,payload.page).then(res=>{
-		        if(res.status === 0){
-					for(let i=0;i<res.data.list.length;i++){
-						res.data.list[i].istochoose = true
-						res.data.list[i].realPrice = res.data.list[i].realPrice * 0.01
-					}
-		            conText.commit("GET_CART",{cartsgoods:res.data.list});
-					if(payload.success){
-						payload.success(res);
-					}
-		        }
-		    })
-		},
-		//购物车分页
-		getCartGoodsPage(conText,payload){
-		    getCartData(conText.rootState.user.uid,payload.page).then(res=>{
-		        if(res.status === 0){
-					for(let i=0;i<res.data.list.length;i++){
-						res.data.list[i].istochoose = true
-						res.data.list[i].realPrice = res.data.list[i].realPrice * 0.01
-					}
-		            conText.commit("ADD_CARTPAGE",{cartsgoods:res.data.list});
-					if(payload.success){
-						payload.success(res);
-					}
-		        }
-		    })
-		},
+    actions: {
+        getCartGoods(conText, payload) {
+            getCartData(conText.rootState.user.uid, payload.page).then(res => {
+                if (res.status === 0) {
+                    for (let i = 0; i < res.data.list.length; i++) {
+                        res.data.list[i].istochoose = true
+                        res.data.list[i].realPrice = res.data.list[i].realPrice * 0.01
+                    }
+                    conText.commit("GET_CART", {cartsgoods: res.data.list});
+                    if (payload.success) {
+                        payload.success(res);
+                    }
+                }
+            })
+        },
+        //购物车分页
+        getCartGoodsPage(conText, payload) {
+            getCartData(conText.rootState.user.uid, payload.page).then(res => {
+                if (res.status === 0) {
+                    for (let i = 0; i < res.data.list.length; i++) {
+                        res.data.list[i].istochoose = true
+                        res.data.list[i].realPrice = res.data.list[i].realPrice * 0.01
+                    }
+                    conText.commit("ADD_CARTPAGE", {cartsgoods: res.data.list});
+                    if (payload.success) {
+                        payload.success(res);
+                    }
+                }
+            })
+        },
 
 
-
-	},
-
+    },
 
 
-    getters:{
+    getters: {
         //计算总金额
         total(state) {
-            if (state.cartsgoods.length>0) {
-                let total=0;
-                for(let i=0;i<state.cartsgoods.length;i++){
-                    if(state.cartsgoods[i].istochoose){
-                        total+=state.cartsgoods[i].realPrice*state.cartsgoods[i].addedNum;
+            if (state.cartsgoods.length > 0) {
+                let total = 0;
+                for (let i = 0; i < state.cartsgoods.length; i++) {
+                    if (state.cartsgoods[i].istochoose) {
+                        total += state.cartsgoods[i].realPrice * state.cartsgoods[i].addedNum;
                     }
                 }
 
                 return total.toFixed(2);
-            }else {
+            } else {
                 return 0.00;
             }
         },
         //运费
-        freight(state){
-            if(state.cartData.length>0){
-                let freights=[];
-                for(let i=0;i<state.cartData.length;i++){
-                    if(state.cartData[i].checked) {
+        freight(state) {
+            if (state.cartData.length > 0) {
+                let freights = [];
+                for (let i = 0; i < state.cartData.length; i++) {
+                    if (state.cartData[i].checked) {
                         freights.push(state.cartData[i].freight);
                     }
                 }
-                return freights.length>0?Math.max.apply(null,freights):0;
-            }else{
+                return freights.length > 0 ? Math.max.apply(null, freights) : 0;
+            } else {
                 return 0;
             }
         }
